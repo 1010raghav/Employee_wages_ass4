fe4b35973028ae510ed714ff432b54b0fba2e3f7
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-23 21:24:39 +0530

    Main Program File

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Day3_emp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UC1_EmpAttendance.Attendance();
+            UC2_EmpAttWages.getAttendance();
+            UC3_EmpWages.getAttendance();
+            UC4_EmpWagesSwitchCase.getAttendance();
+            UC5_CalculateWagesFor20DaysInMonth.getAttendance();
+
+        }
+    }
+}
